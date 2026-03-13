2bce67e7ce47ee9f9d6e575461d81594e41dea41
AlessandroR07 <alerossi0107@gmail.com>
2026-03-13 11:20:35 +0100

    Commit

--- src/main/java/org/example/ClientUDP.java
+++ src/main/java/org/example/ClientUDP.java
@@ -1,4 +1,61 @@
-package org.example;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class ClientUDP {
+
+    public static void main(String[] args) {
+
+        try {
+
+            DatagramSocket socket = new DatagramSocket();
+
+            InetAddress serverAddress = InetAddress.getByName("localhost");
+
+            BufferedReader tastiera =
+                    new BufferedReader(new InputStreamReader(System.in));
+
+            while (true) {
+
+                System.out.print("CLIENT: ");
+                String messaggio = tastiera.readLine();
+
+                byte[] bufferInvio = messaggio.getBytes();
+
+                DatagramPacket packetInvio =
+                        new DatagramPacket(
+                                bufferInvio,
+                                bufferInvio.length,
+                                serverAddress,
+                                3000);
+
+                socket.send(packetInvio);
+
+                byte[] bufferRicezione = new byte[1024];
+
+                DatagramPacket packetRicevuto =
+                        new DatagramPacket(bufferRicezione, bufferRicezione.length);
+
+                socket.receive(packetRicevuto);
+
+                String risposta = new String(
+                        packetRicevuto.getData(),
+                        0,
+                        packetRicevuto.getLength());
+
+                System.out.println("SERVER: " + risposta);
+
+                if (messaggio.equalsIgnoreCase("exit"))
+                    break;
+            }
+
+            socket.close();
+            System.out.println("Client chiuso");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/org/example/ServerUDP.java
+++ src/main/java/org/example/ServerUDP.java
@@ -1,4 +1,52 @@
-package org.example;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
 
 public class ServerUDP {
+
+    public static void main(String[] args) {
+
+        try {
+
+            DatagramSocket socket = new DatagramSocket(3000);
+            System.out.println("Server UDP in ascolto sulla porta 3000");
+
+            byte[] buffer = new byte[1024];
+
+            while (true) {
+
+                DatagramPacket packetRicevuto =
+                        new DatagramPacket(buffer, buffer.length);
+
+                socket.receive(packetRicevuto);
+
+                String messaggio = new String(
+                        packetRicevuto.getData(),
+                        0,
+                        packetRicevuto.getLength());
+
+                System.out.println("CLIENT: " + messaggio);
+
+                // echo (rimanda lo stesso messaggio)
+                byte[] risposta = messaggio.getBytes();
+
+                DatagramPacket packetInvio =
+                        new DatagramPacket(
+                                risposta,
+                                risposta.length,
+                                packetRicevuto.getAddress(),
+                                packetRicevuto.getPort());
+
+                socket.send(packetInvio);
+
+                if (messaggio.equalsIgnoreCase("exit"))
+                    break;
+            }
+
+            socket.close();
+            System.out.println("Server chiuso");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
